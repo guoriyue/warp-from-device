4ef329d1d6cbbd88f755645be89036da5f142f08
YangZhou08 <1800012773@pku.edu.cn>
2024-02-04 22:04:34 -0800

    clear

--- call_wp_max_block.cu
+++ call_wp_max_block.cu
@@ -55,30 +55,3 @@ int main(void)
     printf("%f\n", dest[0]);
     return 0;
 }
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink -o memset.out /rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-
-// nvcc -o call.out --device-link wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu --verbose
-
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink -o cptx /rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-
-// nvcc -arch=sm_70 -o call_ptx_from_device.o -c /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink /rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-
-
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink -o cptx.out /rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.fatbin call_ptx_from_device.o
-
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink -o cptx wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-
-// You should be able to run ptx code directly from the cuda driver api with cuModuleLoadDataEx. There is an example here at page 5
-
-// fatbinary --create="/rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.fatbin" --image3=kind=ptx,file=wp___main__.sm70.ptx,sm=70
-
-
-// nvcc -arch=sm_70 -Xptxas="-v" -dlink -o cptx wp___main__.fatbin /rscratch/zhendong/mfguo/warp/warp_cpp/call_ptx_from_device.cu
-// ptxas --gpu-name sm_70 --verbose --output-file wp_ptxas_compile.ptx /rscratch/zhendong/mfguo/warp/warp_cpp/wp___main__.sm70.ptx
-
-// gcc "call_ptx_from_device.cu" -o "call_ptx_from_device.cpp1.ii" 
-
-// nvcc -dryrun call_ptx_from_device.cu  -rdc=true -lcudadevrt
-// nvcc call_ptx_from_device.cu  -rdc=true -lcudadevrt
