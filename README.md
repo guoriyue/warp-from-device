d8f7b43ae05b38c171a2fff8e75c6ce584fff859
YangZhou08 <1800012773@pku.edu.cn>
2024-02-04 19:26:56 -0800

    float add working

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-
-# warp_cpp
-Examples calling [NVIDIA Warp](https://github.com/nvidia/warp) precompiled (cached) kernels directly from C++ (without Python)
-
-## Usage
+Call [NVIDIA Warp](https://github.com/nvidia/warp) kernels from device
 
 Install Warp
 
@@ -14,66 +10,32 @@ python build_lib.py --cuda_path=/usr/local/warp
 pip install -e .
 ```
 
-
-```
-mkdir cache
-export CACHE_PATH=cache/
-```
-
-example_add_float_array.py has this Warp kernel:
-```
-@wp.kernel
-def add_float_arrays(dest: wp.array(dtype=wp.float32),
-             a: wp.array(dtype=wp.float32),
-             b: wp.array(dtype=wp.float32)):
-
-    tid = wp.tid()
-    dest[tid] = a[tid]+b[tid]
-```
 Run this Warp Python example to jit compile the example_add_float_array.py
 ```
-python example_add_float_array.py
-Warp 0.8.2 initialized:
-   CUDA Toolkit: 11.8, Driver: 12.1
-   Devices:
-     "cpu"    | Intel64 Family 6 Model 186 Stepping 2, GenuineIntel
-     "cuda:0" | NVIDIA GeForce RTX 4090 Laptop GPU (sm_89)
-   Kernel cache: C:\Users\erwin\AppData\Local\NVIDIA Corporation\warp\Cache\0.8.2
-Module __main__ load on device 'cpu' took 15.43 ms
-dest.numpy()= [100.5      101.98572  103.47143  104.95714  106.442856 107.92857
- 109.41428  110.9     ]
- ```
-Note the Kernel cache path, it will contain the compiled Warp kernel as CPU DLL or CUDA PTX binary.
-
-Use cmake, compile and run the C++ example_add_float_array_cpu.cpp and example_add_float_array_cuda.cpp
-Use the variable WARP_PATH to point to the location of the Warp source root
-When running example_add_float_array_cpu, pass the location to the .dll / .so Warp compiled kernel file:
-```
-cmake -DWARP_PATH=/home/ecoumans/dev/warp_cpp/warp .
-cmake --build .
-./example_add_float_array_cpu /home/ecoumans/.cache/warp/0.8.2/bin/wp___main__.so
-a:1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8
-b:100 200 300 400 500 600 700 800
-Sum:101.1 202.2 303.3 404.4 505.5 606.6 707.7 808.8
+python3 float_arrays_add.py
 ```
 
-You can extract the Warp kernel C++ signature from the Warp generated c++ code in the cache/gen folder (gen\wp___main__.cpp)
-```
-// CPU entry points
-void (*add_float_arrays_cpu_forward)(launch_bounds_t dim,
-array_t<float32> var_dest,
-array_t<float32> var_a,
-array_t<float32> var_b);
-```
-The array_t and other Warp definitions are in the builtin.h header file.
+Note the Kernel cache path, it will contain the generated CUDA code.
+Copy the CUDA file and save it in current directory.
 
-Same for the cuda version, make sure to change device = "cpu" into device="cuda" to compile to CUDA/PTX.
-Pass the path to the PTX file as first argument:
 ```
-./example_add_float_array_cuda /home/ecoumans/.cache/warp/0.8.2/bin/wp___main__.sm70.ptx
-hello cuda world
-CUDA driver version:12010
-CUDA device count:1
-len=22741
-Sum:101.1 202.2 303.3 404.4 505.5 606.6 707.7 808.8
+nvcc call_wp_from_device.cu -rdc=true -lcudadevrt
+./a.out
+```
+
+
+In call_wp_from_device.cu, we need to include:
+
+```
+#include "warp/warp/native/builtin.h"
+#include "wp_float_arrays_add.cu"
+```
+
+To call the Warp kernel function, we need to convert our data into Warp variables.
+```
+extern "C" __global__ void add_float_arrays_cuda_kernel_forward(
+    wp::launch_bounds_t dim,
+    wp::array_t<wp::float32> var_dest,
+    wp::array_t<wp::float32> var_a,
+    wp::array_t<wp::float32> var_b)
 ```
--- call_wp_from_device.cu
+++ call_wp_from_device.cu
@@ -1,70 +1,60 @@
 #include <stdio.h>
 #include "warp/warp/native/builtin.h"
-
-extern "C" __global__ void add_float_arrays_cuda_kernel_forward(
-    wp::launch_bounds_t dim,
-    wp::array_t<wp::float32> var_dest,
-    wp::array_t<wp::float32> var_a,
-    wp::array_t<wp::float32> var_b);
+#include "wp_float_arrays_add.cu"
+// extern "C" __global__ void add_float_arrays_cuda_kernel_forward(
+//     wp::launch_bounds_t dim,
+//     wp::array_t<wp::float32> var_dest,
+//     wp::array_t<wp::float32> var_a,
+//     wp::array_t<wp::float32> var_b);
 
 __global__
-void my_kernel(wp::launch_bounds_t dim,
-    wp::array_t<wp::float32> var_dest,
-    wp::array_t<wp::float32> var_a,
-    wp::array_t<wp::float32> var_b)
-{
-    printf("var_dest.data[0]: %d\n", var_dest.data[0]);
-    printf("var_dest.data[1]: %d\n", var_dest.data[1]);
-    printf("var_dest.data[2]: %d\n", var_dest.data[2]);
-    add_float_arrays_cuda_kernel_forward<<<1,1>>>(dim, var_dest, var_a, var_b);
-    printf("var_dest.data[0]: %d\n", var_dest.data[0]);
-    printf("var_dest.data[1]: %d\n", var_dest.data[1]);
-    printf("var_dest.data[2]: %d\n", var_dest.data[2]);
-}
-
-
-int main(void)
+void my_kernel(float* dest, float* a, float* b)
 {
     int num_items = 8;
     wp::launch_bounds_t bounds_cuda;
     bounds_cuda.ndim = 1;
-    bounds_cuda.shape[0] = num_items;//??
+    bounds_cuda.shape[0] = num_items;
     bounds_cuda.size = num_items;
 
     wp::array_t<float> var_dest, var_a, var_b;
 
-    float a[8] = {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8};
-    float b[8] = {100.,200.,300.,400.,500.,600.,700.,800.};
-    float c[8] = {-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.};
-
-    float* cumem_a;
-    float* cumem_b;
-    float* cumem_c;
-    cudaMalloc((void**)&cumem_a, sizeof(float) * 8);
-    cudaMalloc((void**)&cumem_b, sizeof(float) * 8);
-    cudaMalloc((void**)&cumem_c, sizeof(float) * 8);
-    cudaMemcpy(cumem_a, a, sizeof(float) * 8, cudaMemcpyHostToDevice);
-    cudaMemcpy(cumem_b, b, sizeof(float) * 8, cudaMemcpyHostToDevice);
-    cudaMemcpy(cumem_c, c, sizeof(float) * 8, cudaMemcpyHostToDevice);
-
     var_dest.ndim = 1;
     var_dest.shape[0] = num_items;
     var_dest.strides[0] = sizeof(float);
-    var_dest.data = cumem_c;
+    var_dest.data = dest;
 
     var_a.ndim = 1;
     var_a.shape[0] = num_items;
     var_a.strides[0] = sizeof(float);
-    var_a.data = cumem_a;
+    var_a.data = a;
 
     var_b.ndim = 1;
     var_b.shape[0] = num_items;
     var_b.strides[0] = sizeof(float);
-    var_b.data = cumem_b;
+    var_b.data = b;
 
-    my_kernel<<<1, 1>>>(bounds_cuda, var_dest, var_a, var_b);
-    cudaMemcpy(c, var_dest.data, sizeof(float) * 8, cudaMemcpyDeviceToHost);
-    printf("%f %f %f %f %f %f %f %f\n", c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]);
+    add_float_arrays_cuda_kernel_forward<<<1,1>>>(bounds_cuda, var_dest, var_a, var_b);
+}
+
+int main(void)
+{
+    float a[8] = {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8};
+    float b[8] = {100.,200.,300.,400.,500.,600.,700.,800.};
+    float dest[8] = {-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.};
+
+    float* cumem_a;
+    float* cumem_b;
+    float* cumem_dest;
+    cudaMalloc((void**)&cumem_a, sizeof(float) * 8);
+    cudaMalloc((void**)&cumem_b, sizeof(float) * 8);
+    cudaMalloc((void**)&cumem_dest, sizeof(float) * 8);
+    cudaMemcpy(cumem_a, a, sizeof(float) * 8, cudaMemcpyHostToDevice);
+    cudaMemcpy(cumem_b, b, sizeof(float) * 8, cudaMemcpyHostToDevice);
+    cudaMemcpy(cumem_dest, dest, sizeof(float) * 8, cudaMemcpyHostToDevice);
+
+    my_kernel<<<1, 1>>>(cumem_dest, cumem_a, cumem_b);
+    cudaMemcpy(dest, cumem_dest, sizeof(float) * 8, cudaMemcpyDeviceToHost);
+    printf("%f %f %f %f %f %f %f %f\n", dest[0], dest[1], dest[2], dest[3], dest[4], dest[5], dest[6], dest[7]);
 
     return 0;
 }
